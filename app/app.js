1c66c89a67e494464762d999c97be1ea2b771487
Julian_Chu <walkingice@0xlab.org>
2013-09-01 10:29:09 +0800

    todo -> list
    remove ToDo, use simple List

--- app/app.js
+++ app/app.js
@@ -5,14 +5,14 @@ App = angular.module('app', ['ngCookies', 'ngResource', 'app.controllers', 'app.
 
 App.config([
 	'$routeProvider', '$locationProvider', function($routeProvider, $locationProvider, config) {
-		$routeProvider.when('/todo', {
-			templateUrl: '/partials/todo.html'
+		$routeProvider.when('/list', {
+			templateUrl: '/partials/list.html'
 		}).when('/view1', {
 			templateUrl: '/partials/partial1.html'
 		}).when('/about', {
 			templateUrl: '/partials/about.html'
 		}).otherwise({
-			redirectTo: '/todo'
+			redirectTo: '/list'
 		});
 		return $locationProvider.html5Mode(false);
 	}
