a3f107aa2c1018eecfcfa370a9c72de69dfa74d3
Julian_Chu <walkingice@0xlab.org>
2016-02-24 20:47:51 +0800

    build: add webpack config for building process

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+var path = require('path');
+
+var output = '_public';
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var extractLESS = new ExtractTextPlugin('app.css');
+
+// https://webpack.github.io/docs/configuration.html
+module.exports = {
+    context: __dirname + '/app',
+    entry: {
+        app: ['./js/app.jsx'],
+        vendor: ['react', 'react-dom', 'react-router'],
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loaders: ['react-hot', 'babel?presets[]=react,presets[]=es2015']
+            }, {
+                test: /\.json$/,
+                loader: 'json'
+            }, {
+                test: /\.(css|less)$/,
+                loader: extractLESS.extract(['css', 'less'])
+            }, {
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: "file-loader"
+            }, {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: "url-loader?limit=10000&minetype=application/font-woff"
+            }
+        ]
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, output)
+    },
+    plugins: [
+        extractLESS,
+        new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor",
+                                                /* filename= */"vendor.js")
+    ]
+}
